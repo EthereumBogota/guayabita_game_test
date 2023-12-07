a9a59dd21dda7d8477d1ad406dd856e9be241b8d
oscar50 <oscar@whaleandjaguar.co>
2023-12-06 23:06:43 -0500

    feat: mumbai connection

---
+++ web3connect.py
@@ -0,0 +1,51 @@
+import os
+import json
+from web3 import Web3
+
+# with open("abi.json") as archivo:
+#     ABI = json.load(archivo)
+with open("abi.json") as f:
+    info_json = json.load(f)
+ABI = info_json["output"]["abi"]
+
+CONTRACT = os.environ["CONTRACT"]
+WALLET = os.environ["WALLET"]
+PRIV_KEY = os.environ["PRIV_KEY"]
+
+polygon_rpc_url = 'https://endpoints.omniatech.io/v1/matic/mumbai/public'
+w3 = Web3(Web3.HTTPProvider(polygon_rpc_url))
+
+if w3.is_connected():
+    print("-" * 50)
+    print("Connection Successful")
+    print("-" * 50)
+else:
+    print("Connection Failed")
+
+contract_address = CONTRACT
+contract_abi = ABI
+
+contract = w3.eth.contract(address=contract_address, abi=contract_abi)
+
+account_address = WALLET
+private_key = PRIV_KEY
+
+result = contract.functions.leerSaludo().call()
+print("Resultado de la consulta:", result)
+
+# Interactuar con la función guardarSaludo
+greeting_message = "Saludos desde la casa con Milo"
+function_data = contract.functions.guardarSaludo(greeting_message).build_transaction({
+    'from': account_address,
+    'gas': 3000000,
+    'gasPrice': w3.to_wei('10', 'gwei'),
+    'nonce': w3.eth.get_transaction_count(account_address),
+    'chainId': 80001,
+})
+
+signed_transaction = w3.eth.account.sign_transaction(function_data, private_key)
+transaction_hash = w3.eth.send_raw_transaction(signed_transaction.rawTransaction)
+
+print("Hash de la transacción enviada:", transaction_hash.hex())
+
+# https://mumbai.polygonscan.com/tx/0x663fcce44e2507b9a0fb49b194149670b10fec18442d3a5b2234a682b2784a54
